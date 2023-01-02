2e7a1d74ad467edd24f0075433534704c13172d7
Doris Liu <chiayuu.liu@gmail.com>
2023-01-02 15:04:03 +0800

    首頁更新

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import './Style/App.css';
+import "./Style/App.css";
 import { Route, Routes } from "react-router-dom";
-import Layout from './Component/Layout/Layout';
-import WeatherPage from './Pages/WeatherPage';
-import PlayerPage from './Pages/PlayerPage';
-import PlayerDetail from './Pages/PlayerDetail';
-import Home from './Pages/Home';
-
+import Layout from "./Component/Layout/Layout";
+import WeatherPage from "./Pages/WeatherPage";
+import PlayerPage from "./Pages/PlayerPage";
+import PlayerDetail from "./Pages/PlayerDetail";
+import Home from "./Pages/Home";
+import { createTheme } from "@mui/material/styles";
+export const theme = createTheme({
+  palette: {
+    // 背景白
+    lightBrown: {
+      main: "#d9bca3",
+    },
+    // 主咖
+    midBrown: {
+      main: "#bf8360",
+    },
+    // 深咖
+    darkBrown: {
+      main: "#73463c",
+    },
+  },
+});
 function App() {
   return (
     // <WeatherPage />
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-
       </Route>
       <Route path="/weather" element={<WeatherPage />} />
       <Route path="/playerData" element={<PlayerPage />} />
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,58 @@
-import React from 'react'
-
+import React from "react";
+import { Container, Box } from "@mui/material/";
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="Page">
+      <div class="banner">
+        <div class="infowrap">
+          <div class="info d-flex">
+            <div class="pic animate__animated animate__fadeInLeft">
+              <img src="http://localhost:3000/images/Profile/avatar.jepg" alt="" />
+              {/* http://chiayuuliu.github.io/myWebsite/images/weather/clear.svg */}
+              {/* <img
+                src="http://chiayuuliu.github.io/myWebsite/images/Profile/avatar.jpeg"
+                alt=""
+              /> */}
+            </div>
+            <div class="intro animate__animated animate__fadeInRight">
+              <h2 class="name">劉家佑 Doris Liu</h2>
+              <p>Web Front Engineer</p>
+              <div>
+                <span>Email : </span>
+                <a href="mailto:chiayuu.liu@gmail.com">chiayuu.liu@gmail.com</a>
+              </div>
 
-export default Home
+              <div>
+                <span>Phone : </span>0953-680-007
+              </div>
+              <div>
+                <span>City : </span>NewTaipei City
+              </div>
+            </div>
+          </div>
+          <div class="iconwrap">
+            <a
+              href="https://github.com/chiayuuliu"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i class="fab fa-github"></i>
+            </a>
+            <a
+              href="https://www.cakeresume.com/s--eQy6zafab9NGIQcwUb1vQA--/chiayuu-liu-f73e98"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i class="fas fa-address-card"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+      <Container maxWidth="lg">
+        <Box sx={{ bgcolor: "#cfe8fc", height: "100vh" }} />
+      </Container>
+    </div>
+  );
+};
+
+export default Home;
